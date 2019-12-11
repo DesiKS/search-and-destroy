6cc62850cc577072c67c819077e36fee63cf225a
Desislava Shunturova <shunturova@gmail.com>
2019-12-11 18:02:15 -0500

    jump halfway done

--- min-jumps/min-jumps.js
+++ min-jumps/min-jumps.js
@@ -1,8 +1,21 @@
 'use strict';
 
 // Complete this algo
-const minJumps = arr => {
+const minJumps = (arr) => {
+    
+    
 
 };
+
+function helper(currentIndex, arr) {
+    let value = arr[currentIndex]
+    let resultantArr = arr.slice(currentIndex+1, currentIndex+value+1)
+    let maxIndexValArr = resultantArr.map((el, index) => {
+        return el + index
+    })
+    let maxIndexPlusVal = Math.max(...maxIndexValArr)
+    let intermediateIndex = maxIndexValArr.indexOf(maxIndexPlusVal)
+    return currentIndex+intermediateIndex+1
+}
 
 module.exports = minJumps
